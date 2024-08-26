e472256ae218eebd9edea0a23d93fe96e2723bca
Zhengyu Zou <113187557+zzou9@users.noreply.github.com>
2024-08-26 16:34:00 -0400

    fixed bugs on the module panel of 31 map module

--- src/31-map/sketch.js
+++ src/31-map/sketch.js
@@ -60,7 +60,7 @@ function setup() {
     actionPanel.showPanel = false;
     trajPanel = new TrajectoryPanel(10, actionPanel.y+actionPanel.h+10, polygon);
     infoPanel = new InfoPanel(2*xT - 210, 10, polygon, map);
-    modulePanel = new ModulePanel(xT-200, 40, "31-Map", color.BLACK);
+    modulePanel = new ModulePanel("31-Map");
 }
 
 function draw() {
